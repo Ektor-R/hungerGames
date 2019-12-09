2c4b28f53958e4b7fe32b032165066821bc1cb19
Ektor-R <55707708+Ektor-R@users.noreply.github.com>
2019-12-09 18:49:52 +0200

    Variables, Getters-Setters

--- src/main/MinMaxPlayer.java
+++ src/main/MinMaxPlayer.java
@@ -1,5 +1,28 @@
 package main;
 
-public class MinMaxPlayer {
+import java.util.ArrayList;
+
+public class MinMaxPlayer extends Player {
+
+    //local variables
+    ArrayList<Integer[]> path = new ArrayList<Integer[]>();
+	
+	//constructors
+	public MinMaxPlayer() {
+		
+	}
+	public MinMaxPlayer(ArrayList<Integer[]> path) {
+		this.path = path;
+	}
+	
+	//getters
+	public ArrayList<Integer[]> getPath(){
+		return path;
+	}
+	
+	//setters
+	public void setPath(ArrayList<Integer[]> path) {
+		this.path = path;
+	}
 
 }
--- src/main/Node.java
+++ src/main/Node.java
@@ -1,5 +1,60 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Node {
+    
+    //local variables
+    private Node parent;
+    private ArrayList<Node> children = new ArrayList<Node>();
+    private int nodeDepth;
+    private int[] nodeMove = new int[3];
+    private Board nodeBoard;
+    private double nodeEvaluation;
+
+    //constructors 
+    public Node(){
+
+    }
+
+    //getters
+    public Node getParent(){
+        return parent;
+    }
+    public ArrayList<Node> getChildren(){
+        return children;
+    }
+    public int getNodeDepth(){
+        return nodeDepth;
+    }
+    public int[] getNodeMove(){
+        return nodeMove;
+    }
+    public Board getNodeBoard(){
+        return nodeBoard;
+    }
+    public double getNodeEvaluation(){
+        return nodeEvaluation;
+    }
+
+    //setters
+    public void setParent(Node parent){
+        this.parent = parent;
+    }
+    public void setChildren(ArrayList<Node> children){
+        this.children = children;
+    }
+    public void setNodeDepth(int nodeDepth){
+        this.nodeDepth = nodeDepth;
+    }
+    public void setNodeMove(int[] nodeMove){
+        this.nodeMove = nodeMove;
+    }
+    public void setNodeBoard(Board nodeBoard){
+        this.nodeBoard = nodeBoard;
+    }
+    public void setNodeEvaluation(double nodeEvaluation){
+        this.nodeEvaluation = nodeEvaluation;
+    }
 
 }
